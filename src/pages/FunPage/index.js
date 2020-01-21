ed1f61909cfc6640ade4e08f4eace1464900eb4b
Frederica Blissett aka Samri <redricasa@users.noreply.github.com>
2020-01-21 11:45:09 -0800

    the css was imported to have the weather info in white to show up

--- src/pages/FunPage/index.js
+++ src/pages/FunPage/index.js
@@ -2,7 +2,7 @@ import React, { useState, Component } from "react";
 // import TaskList from "../../components/TaskList";
 // import TaskForm from "../../components/TaskForm";
 // import Alert from "../../components/Alert";
-// import App from "../../App.css";
+import App from "../../App.css";
 import uuid from "uuid/v4";
 import "bootstrap/dist/css/bootstrap.min.css";
 // import Nav from "../../components/Nav";
